36c807aa98bd9b95eb2dfa9f61583216dfa7f1f1
Doha Kaddaf <doha_kaddaf@carrefour.com>
2024-05-04 10:52:45 +0200

    lesson 12 second part

--- lesson-12/index.js
+++ lesson-12/index.js
@@ -8,3 +8,41 @@ const dogElement = document.querySelector('.mouse');
 const pElement = document.createElement('p');
 pElement.innerText = 'Hello World';
 dogElement.appendChild(pElement);
+
+
+const mouseElement = document.getElementsByClassName('mouse');
+console.log(mouseElement);
+[...mouseElement].forEach((element) => {
+    const pElement = document.createElement('p');
+    pElement.innerText = 'Hello World';
+    element.appendChild(pElement);
+});
+
+[...document.querySelectorAll('.mouse')].map((element) => {
+    element.setAttribute('style', 'background-color: red; color: white; width: 100px; height: 100px;margin: 10px; padding: 10px;');
+})
+
+
+const allDivs = document.querySelectorAll('div, .mouse, #cat');
+
+
+const allDivsArray = [...allDivs];
+
+allDivsArray.map((div) => {
+    if(div.className === 'mouse') {
+        div.style.width = '200px';
+        div.style.height = '200px';
+        div.style.backgroundColor = 'orange';
+        div.style.color = 'white';
+        div.style.margin = '10px';
+        div.style.padding = '10px';
+        div.textContent = 'Cats are cute!';
+        div.innerHTML = '<p style="color:brown"> <span style="font-size: 30px">Cats</span> are cute!</h1>';
+        //div.setAttribute('style', 'background-color: blue; color: white; width: 100px; height: 100px;margin: 10px; padding: 10px;');
+    }
+    else {
+        div.setAttribute('style', 'background-color: red; color: white; width: 100px; height: 100px;margin: 10px; padding: 10px;');
+    }
+})
+
+console.log(allDivs);
