30b444713a3607c252966f8d56872b7f5d924c40
Doha Kaddaf <doha_kaddaf@carrefour.com>
2024-05-04 09:56:49 +0200

    lesson 12

---
+++ lesson-12/index.js
@@ -0,0 +1,10 @@
+const catElement = document.getElementById('cat');
+const h2Element = document.createElement('h2');
+h2Element.innerText = 'Hello World';
+catElement.appendChild(h2Element);
+
+
+const dogElement = document.querySelector('.mouse');
+const pElement = document.createElement('p');
+pElement.innerText = 'Hello World';
+dogElement.appendChild(pElement);
