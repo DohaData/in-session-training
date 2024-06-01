87012ed7979cf8022376c8d3acdc4533722b1d6d
Doha Kaddaf <doha.kaddaf@gmail.com>
2024-06-01 10:39:01 +0200

    add more handlebars functions

--- module-02/lesson-03/express-hbs-demo/app.js
+++ module-02/lesson-03/express-hbs-demo/app.js
@@ -8,10 +8,14 @@ app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res, next) => {
+    console.log("Request IP address: ", req.ip);
     let data = {
         first_favorite: "JavaScript",
         second_favorite: "Node.js",
         third_favorite: "Express.js",
+        showFourthFavorite: true,
+        fourth_favorite: "React",
+        cities: ["New York", "San Francisco", "Austin", "Seattle"],
     };
   res.render("index", {
     title: "My favorite programming languages",
