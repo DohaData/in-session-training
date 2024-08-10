9aace3f900b22647fa1b80d16f6f6d7e0bbb5955
Doha Kaddaf <doha.kaddaf@gmail.com>
2024-08-10 12:00:50 +0200

    add stars

--- module-03/lesson-03/react-lists/src/components/MovieCard.jsx
+++ module-03/lesson-03/react-lists/src/components/MovieCard.jsx
@@ -1,4 +1,11 @@
 function MovieCard(props) {
+    function generateStars(rating) {
+      let stars = "";
+      for (let i = 0; i < Math.round(rating); i++) {
+        stars += "⭐️";
+      }
+      return stars;
+    }
   return (
     <div key={props.movie._id}>
       <h2>{props.movie.title}</h2>
@@ -6,7 +13,7 @@ function MovieCard(props) {
       {/* {props.movie.hasOscars && <p>Got the Oscar Award! </p>}
       {!props.movie.hasOscars && <p>Great movie but no Oscars! </p>} */}
       {props.movie.hasOscars ? <p>Got the Oscar Award! </p> : <p>Great movie but no Oscars!</p>}
-      <p>{props.movie.IMDBRating}</p>
+      <p>{generateStars(props.movie.IMDBRating)}</p>
       <button onClick={() => props.updateMovie(props.movie._id)}>Delete</button>
     </div>
   );
